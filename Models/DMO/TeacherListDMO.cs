1c78adc98cda21590f196e8d145ee30ea05d5c61
aycaag <100555017+aycaag@users.noreply.github.com>
2024-12-16 03:42:55 +0300

    Stop tracking files listed in .gitignore

--- Models/DMO/TeacherListDMO.cs
+++
@@ -1,7 +0,0 @@
-public class TeacherListDMO{
-    public int ID { get; set; } 
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string Lesson { get; set;}   
-
-}
--- Models/DTO/TeacherListDTO.cs
+++
@@ -1,9 +0,0 @@
-public class TeacherListDTO
-{
-    public int ID { get; set; } 
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string Lesson { get; set;}   
-
-
-}
