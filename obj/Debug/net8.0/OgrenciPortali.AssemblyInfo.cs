2b363ca3b05a78c9612108a4c03ece78084c375c
aycaag <100555017+aycaag@users.noreply.github.com>
2024-12-16 03:17:27 +0300

    test

--- obj/Debug/net8.0/OgrenciPortali.AssemblyInfo.cs
+++ obj/Debug/net8.0/OgrenciPortali.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OgrenciPortali")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178d4e7767264f366e90ac84de354644b5d4cb5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c77a0244bf4ddd7249f004cc080a96a6ae0786")]
 [assembly: System.Reflection.AssemblyProductAttribute("OgrenciPortali")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OgrenciPortali")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
